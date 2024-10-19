d2e2b82962494d384aa38191345b8067447965bd
Fazil <faziltirtana@tutanota.com>
2024-10-19 18:19:42 +0700

    feat: create authentication module

--- rasapalembang/urls.py
+++ rasapalembang/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path
-from .views import (
+from authentication.views import (
     profile,
     signup,
     login,
