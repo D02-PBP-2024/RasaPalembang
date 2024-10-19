1b1a7c1f79e92025be2ed7f6df37ebccb25bd077
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2024-10-20 00:48:26 +0700

    feat: create site route

--- minuman/views.py
+++ minuman/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from minuman.models import Minuman
+from minuman.forms import MinumanForm
+
 
 # Create your views here.
+def show_minuman(request):
+    minuman = Minuman.objects.all()
+    context = {"minuman": minuman}
+    return render(request, "minuman/index.html", context)
+
+
+def show_minuman_by_id(request, id):
+    # minuman = Minuman.objects.get(pk=id)
+    context = {
+        # "minuman": minuman,
+        "id": id,
+    }
+    return render(request, "minuman_by_id/index.html", context)
+
+
+def create_minuman(request):
+    if request.method == "POST":
+        form = MinumanForm(request.POST, request.FILES)
+        if form.is_valid():
+            minuman = form.save(commit=False)
+            # minuman.restoran = request.restoran
+            minuman.save()
+            return redirect("minuman:show_minuman")
+    else:
+        form = MinumanForm()
+
+    context = {"form": form}
+    return render(request, "create/index.html", context)
+
+
+def edit_minuman(request, id):
+    # minuman = Minuman.objects.get(pk=id)
+    context = {
+        # 'minuman': minuman,
+        'id': id,
+    }
+    return render(request, "edit/index.html", context)
+
+
+def delete_minuman(request, id):
+    # minuman = Minuman.objects.get(pk=id)
+
+    return HttpResponseRedirect(reverse("minuman:show_minuman"))
