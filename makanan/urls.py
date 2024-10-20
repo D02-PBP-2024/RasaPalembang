ebd4320e77a5447ab3300bcf18dd8c044de26fda
AthallahD <athallah2209@gmail.com>
2024-10-20 16:50:08 +0700

    feat: create and update html for show, edit, tambah makanan, views, and urls

--- makanan/urls.py
+++ makanan/urls.py
@@ -5,7 +5,8 @@ app_name = 'makanan'
 
 urlpatterns = [
     path('', show_makanan, name='show_makanan'),
-    path('create/', create_makanan, name='create_makanan'),
-    path('makanan/<uuid:id>/', show_makanan_by_id, name='show_makanan_by_id')
-    
+    path('tambah/', tambah_makanan, name='tambah_makanan'),
+    path('<uuid:id>/', show_makanan_by_id, name='show_makanan_by_id'),
+    path('<uuid:id>/edit/', edit_makanan, name='edit_makanan'),
+    path('<uuid:id>/delete/', delete_makanan, name='delete_makanan'),
 ]
--- makanan/views.py
+++ makanan/views.py
@@ -1,25 +1,49 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from makanan.forms import MakananForm, KategoriForm
 from makanan.models import Makanan, Kategori
+from restoran.models import Restoran
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def show_makanan(request):
     makanan = Makanan.objects.all()
     context = {'makanan': makanan}
     return render(request, 'makanan/show_makanan.html', context)
 
-def create_makanan(request):
-    form = MakananForm(request.POST)
+def tambah_makanan(request):
+    restoran = Restoran.objects.filter(user=request.user)
+    if request.method == "POST":
+        form = MakananForm(request.POST, request.FILES)
+        if form.is_valid():
+            minuman = form.save(commit=False)
+            minuman.save()
+            return redirect("minuman:show_minuman")
+    else:
+        form = MakananForm()
 
-    if form.is_valid() and request.method == 'POST':
-        makanan_entry = form.save(commit=False)
-        makanan_entry.restoran = request.user.restoran
-        makanan_entry.save()
-        return redirect('makanan:create_makanan')
-    
-    context = {'form': form}
-    return render(request, 'create/create_makanan.html', context)
+    context = {
+        "form": form,
+        "restoran": restoran,
+    }
+    return render(request, "tambah/tambah_makanan.html", context)
 
 def show_makanan_by_id(request, id):
-    makanan = Makanan.objects.get(pk=id)
-    context = {'makanan': makanan} 
+    makanan = get_object_or_404(Makanan, pk=id)
+    context = {'makanan': makanan}
     return render(request, 'makanan/show_makanan_by_id.html', context)
+
+def edit_makanan(request, id):
+    makanan = get_object_or_404(Makanan, pk=id)
+    form = MakananForm(request.POST or None, instance=makanan)
+
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('makanan:show_makanan_by_id', id=id)
+    
+    context = {'form': form}
+    return render(request, 'makanan/edit_makanan.html', context)
+
+def delete_makanan(request, id):
+    makanan = Makanan.objects.get(pk=id)
+    makanan.delete()
+    return redirect("makanan:show_makanan")
