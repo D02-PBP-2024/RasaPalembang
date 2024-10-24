e3875b63a721ba8615fc90fe63f0317a52630210
Fazil <faziltirtana@tutanota.com>
2024-10-24 16:35:54 +0700

    fix: media not showing

--- rasapalembang/settings.py
+++ rasapalembang/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = "authentication.User"
