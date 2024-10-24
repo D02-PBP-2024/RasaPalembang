66b8bb869c8af072bc85f3281855e8cd65c8fe0b
mdlnecg <mdlnecg@gmail.com>
2024-10-24 22:20:48 +0700

    feat: add tanggal_posting

--- forum/models.py
+++ forum/models.py
@@ -9,6 +9,7 @@ class Forum(models.Model):
 
     topik = models.CharField(max_length=255)
     pesan = models.TextField()
+    tanggal_posting = models.DateTimeField(auto_now_add=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     restoran = models.ForeignKey(Restoran, on_delete=models.CASCADE)
@@ -18,6 +19,7 @@ class Balasan(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     pesan = models.TextField()
+    tanggal_posting = models.DateTimeField(auto_now_add=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
@@ -28,3 +30,4 @@ class Vote(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     balasan = models.ForeignKey(Balasan, on_delete=models.CASCADE)
+    nilai = models.IntegerField(default=0)
