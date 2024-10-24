8c55528ca9fa06e314c8053c4fd00467de01b77a
Fazil <faziltirtana@tutanota.com>
2024-10-24 21:03:51 +0700

    feat(favorit): better template structuring

--- favorit/views.py
+++ favorit/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Favorit
-from .forms import FavoritForm 
+from favorit.forms import FavoritForm
 from django.contrib import messages
+from favorit.models import Favorit
 
 
 @login_required(login_url="/login")
@@ -10,57 +10,58 @@ def show_favorit(request):
     """
     Menampilkan daftar favorit untuk pengguna yang sedang login.
     """
-    favorit_show = Favorit.objects.filter(user=request.user)
-    return render(request, 'favorit/favorit_show.html', {'favorit_show': favorit_show})
+    favorit = Favorit.objects.filter(user=request.user)
+    return render(request, "favorit/favorit/index.html", {"favorit": favorit})
 
 
 @login_required(login_url="/login")
-def add_favorit(request):
+def tambah_favorit(request):
     """
     Menambahkan makanan, minuman, atau restoran ke dalam daftar favorit.
     """
-    if request.method == 'POST':
+    if request.method == "POST":
         form = FavoritForm(request.POST)
         if form.is_valid():
             favorit = form.save(commit=False)
             favorit.user = request.user  # Set pengguna yang sedang login
             favorit.save()
-            messages.success(request, 'Favorit berhasil ditambahkan.')
-            return redirect('show_favorit')
+            messages.success(request, "Favorit berhasil ditambahkan.")
+            return redirect("favorit:show_favorit")
     else:
         form = FavoritForm()
 
-    return render(request, 'favorit/add_favorit.html', {'form': form})
+    return render(request, "favorit/tambah/index.html", {"form": form})
 
 
 @login_required(login_url="/login")
-def edit_favorit(request, favorit_id):
+def ubah_favorit(request, favorit_id):
     """
     Mengedit catatan favorit yang sudah ada.
     """
     favorit = get_object_or_404(Favorit, id=favorit_id, user=request.user)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = FavoritForm(request.POST, instance=favorit)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Favorit berhasil diperbarui.')
-            return redirect('show_favorit')
+            messages.success(request, "Favorit berhasil diperbarui.")
+            return redirect("favorit:show_favorit")
     else:
         form = FavoritForm(instance=favorit)
 
-    return render(request, 'favorit/edit_favorit.html', {'form': form})
+    return render(request, "favorit/ubah/index.html", {"form": form})
 
 
 @login_required(login_url="/login")
-def delete_favorit(request, favorit_id):
+def hapus_favorit(request, favorit_id):
     """
     Menghapus favorit dari daftar.
     """
     favorit = get_object_or_404(Favorit, id=favorit_id, user=request.user)
-    if request.method == 'POST':
-        favorit.delete()
-        messages.success(request, 'Favorit berhasil dihapus.')
-        return redirect('show_favorit')
 
-    return render(request, 'favorit/delete_favorit.html', {'favorit': favorit})
+    if request.method == "POST":
+        favorit.delete()
+        messages.success(request, "Favorit berhasil dihapus.")
+        return redirect("favorit:show_favorit")
+
+    return render(request, "favorit/hapus/index.html", {"favorit": favorit})
