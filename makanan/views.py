5e5bc4c0eeb9d856aab3b736ec3f24bd8991e4f9
AthallahD <athallah2209@gmail.com>
2024-10-24 14:10:31 +0700

    feat: new feature on show_makanan

--- makanan/views.py
+++ makanan/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render, redirect, get_object_or_404
 from makanan.forms import MakananForm
 from makanan.models import Makanan, Kategori
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 def show_makanan(request):
     makanan = Makanan.objects.all()
-    context = {'makanan_list': makanan}
+    paginator = Paginator(makanan, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'makanan_list': makanan,
+        'page_obj': page_obj,
+        'total_page': paginator.num_pages,
+        }
     return render(request, 'makanan/show/show_makanan.html', context)
 
 @login_required(login_url="/login")
@@ -42,13 +50,14 @@ def edit_makanan(request, id):
     if request.user.peran != 'pemilik_restoran' or request.user != makanan.restoran.user:
         return redirect("makanan:edit_makanan")
 
-    form = MakananForm(request.POST or None, instance=makanan)
+    # Tambahkan `request.FILES` untuk menangani file gambar
+    form = MakananForm(request.POST or None, request.FILES or None, instance=makanan)
 
     if form.is_valid() and request.method == 'POST':
-        form.save()
+        form.save()  # Simpan gambar baru atau update field lainnya
         return redirect('makanan:detail_makanan', id=id)
     
-    context = {'form': form}
+    context = {'form': form, 'makanan': makanan}
     return render(request, 'makanan/edit_makanan/edit_makanan.html', context)
 
 @login_required(login_url="/login")
@@ -61,3 +70,15 @@ def delete_makanan(request, id):
     
     makanan.delete()
     return redirect("makanan:show_makanan")
+
+def filter_by_kategori(request, kategori_id):
+    kategori = get_object_or_404(Kategori, pk=kategori_id)
+    makanan_list = Makanan.objects.filter(kategori=kategori)
+    
+    context = {
+        'kategoris': Kategori.objects.all(),
+        'makanan_list': makanan_list,
+        'selected_kategori': kategori
+    }
+
+    return render(request, 'makanan/show_makanan.html', context)
