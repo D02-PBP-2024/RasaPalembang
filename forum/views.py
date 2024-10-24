d354211bf4a69dbb6fa22f8c877be20a4c99f231
mdlnecg <mdlnecg@gmail.com>
2024-10-24 22:23:43 +0700

    feat: add edit_forum and edit_balasan

--- forum/views.py
+++ forum/views.py
@@ -3,15 +3,17 @@ from django.contrib.auth.decorators import login_required
 from forum.models import Forum, Balasan
 from forum.forms import ForumForm, BalasanForm
 from restoran.models import Restoran
+from authentication.models import User
 
 
 def show_forum(request, id_restoran):
-    forum = Forum.objects.filter(restoran=id_restoran)
+    forum = Forum.objects.filter(restoran=id_restoran).select_related("user")
     restoran = Restoran.objects.get(pk=id_restoran)
     context = {
         "forum": forum,
-        "restoran": restoran,
+        "restoran": restoran
         }
+    
     return render(request, "forum/forum_all/index.html", context)
 
 
@@ -40,6 +42,9 @@ def create_forum(request, id_restoran):
         topik.save()
         return redirect('forum:show_forum', id_restoran=restoran.id)
 
+    # request.user.poin += 5
+    # request.user.save()
+
     context = {'form': form}
     return render(request, 'forum/tambah/index.html', context)
 
@@ -50,7 +55,7 @@ def balas(request, id_restoran, id_forum):
     forum = Forum.objects.get(pk=id_forum)
     form = BalasanForm(request.POST or None)
 
-    if request.user.peran != 'Pengulas':
+    if request.user.peran != 'pengulas':
         return redirect('forum:show_forum_by_id', id_restoran=restoran.id, id_forum=forum.id)
 
     if form.is_valid() and request.method == "POST":
@@ -60,11 +65,30 @@ def balas(request, id_restoran, id_forum):
         balasan.save()
         return redirect('forum:show_forum_by_id', id_restoran=restoran.id, id_forum=forum.id)
 
+    # forum.user.poin += 5
+    # forum.user.save()
+
     context = {
         'form': form,
         'forum': forum,
         }
     return render(request, 'forum/balas/index.html', context)
+
+
+@login_required(login_url="login")
+def edit_forum(request, id_restoran, id_forum):
+    forum = Forum.objects.get(pk=id_forum)
+    form = ForumForm(request.POST, instance=forum)
+    restoran = Restoran.objects.get(pk=id_restoran)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('forum:show_forum', id_restoran=restoran.id)
+        
+    context = {"forum": forum}
+        
+    return render(request, 'edit/edit_forum.html', context)
 
 
 @login_required(login_url="login")
@@ -75,13 +99,34 @@ def delete_forum(request, id_restoran, id_forum):
     forum.delete()
     return redirect('forum:show_forum', id_restoran=restoran.id)
 
+
+@login_required(login_url="login")
+def edit_balasan(request, id_restoran, id_forum, id_balasan):
+    forum = Forum.objects.get(pk=id_forum)
+    restoran = Restoran.objects.get(pk=id_restoran)
+    balasan = Balasan.objects.get(pk=id_balasan)
+    form = BalasanForm(request.POST, instance=balasan)
+
+    if request.user.peran != 'pengulas':
+        return redirect('forum:show_forum_by_id', id_restoran=restoran.id, id_forum=forum.id)
+    
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('forum:show_forum_by_id', id_restoran=restoran.id, id_forum=forum.id)
+
+    context = {"balasan": balasan}
+
+    return render(request, "edit/edit_balasan.html", context)
+
+
 @login_required(login_url="login")
 def delete_balasan(request, id_restoran, id_forum, id_balasan):
     forum = Forum.objects.get(pk=id_forum)
     restoran = Restoran.objects.get(pk=id_restoran)
     balasan = Balasan.objects.get(pk=id_balasan)
 
-    if request.user.peran != 'Pengulas':
+    if request.user.peran != 'pengulas':
         return redirect('forum:show_forum_by_id', id_restoran=restoran.id, id_forum=forum.id)
 
     balasan.delete()
