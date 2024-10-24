de114aff69590e9efb21dc6f945fa7403951a477
Fazil <faziltirtana@tutanota.com>
2024-10-24 21:02:12 +0700

    feat(favorit): update url path to bahasa indonesia

--- favorit/urls.py
+++ favorit/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from favorit.views import show_favorit, add_favorit, edit_favorit, delete_favorit
+from favorit.views import show_favorit, tambah_favorit, ubah_favorit, hapus_favorit
 
-app_name = 'favorit'
+app_name = "favorit"
 
 urlpatterns = [
-    path('', show_favorit, name='show_favorit'), # Menampilkan daftar favorit
-    path('add/', add_favorit, name='add_favorit'),  # Menambahkan favorit baru
-    path('edit/<uuid:favorit_id>/', edit_favorit, name='edit_favorit'),  # Mengedit favorit
-    path('delete/<uuid:favorit_id>/', delete_favorit, name='delete_favorit'),  # Menghapus favorit
+    path("", show_favorit, name="show_favorit"),  # Menampilkan daftar favorit
+    path("tambah/", tambah_favorit, name="tambah_favorit"),  # Menambahkan favorit baru
+    path("ubah/<uuid:favorit_id>/", ubah_favorit, name="ubah_favorit"),  # Mengedit favorit
+    path("hapus/<uuid:favorit_id>/", hapus_favorit, name="hapus_favorit"),  # Menghapus favorit
 ]
