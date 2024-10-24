d684d16c0377210d5e8efc5861b43baafa6427f7
AthallahD <athallah2209@gmail.com>
2024-10-24 10:27:14 +0700

    feat: new design on couple of html and update forms and urls

--- makanan/forms.py
+++ makanan/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
+from django import forms
 from makanan.models import Makanan
 
 class MakananForm(ModelForm):
     class Meta:
         model = Makanan
         fields = ['nama', 'harga', 'deskripsi', 'gambar', 'kalori', 'restoran', 'kategori']
+        widgets = {
+            'kategori': forms.CheckboxSelectMultiple(),  # Set kategori jadi checkbox
+        }
--- makanan/urls.py
+++ makanan/urls.py
@@ -5,8 +5,8 @@ app_name = 'makanan'
 
 urlpatterns = [
     path('', show_makanan, name='show_makanan'),
-    path('<uuid:id>/detail/', detail_makanan, name='detail_makanan'),
+    path('<uuid:id>', detail_makanan, name='detail_makanan'),
     path('tambah/', tambah_makanan, name='tambah_makanan'),
-    path('<uuid:id>/edit/', edit_makanan, name='edit_makanan'),
-    path('<uuid:id>/delete/', delete_makanan, name='delete_makanan'),
+    path('<uuid:id>/edit', edit_makanan, name='edit_makanan'),
+    path('<uuid:id>/delete', delete_makanan, name='delete_makanan'),
 ]
