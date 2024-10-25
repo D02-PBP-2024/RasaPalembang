8faf13e69fc9b45a3520f8c6f7c9cb931da30e9a
Fazil <faziltirtana@tutanota.com>
2024-10-26 01:16:40 +0700

    feat: add featured items in landing page

--- rasapalembang/views.py
+++ rasapalembang/views.py
@@ -1,5 +1,36 @@
+from restoran.models import Restoran
 from django.shortcuts import render
+from django.utils import timezone
+import random
 
 
 def landing(request):
-    return render(request, 'landing/index.html', {})
+    restoran_list = random.sample(list(Restoran.objects.all()), 4)
+    current_time = timezone.localtime().time()
+    restoran_with_status = []
+
+    for restoran in restoran_list:
+        jam_buka = restoran.jam_buka
+        jam_tutup = restoran.jam_tutup
+
+        if jam_buka < jam_tutup:
+            if jam_buka <= current_time <= jam_tutup:
+                status = "Buka"
+            else:
+                status = "Tutup"
+        else:
+            if current_time >= jam_buka or current_time <= jam_tutup:
+                status = "Buka"
+            else:
+                status = "Tutup"
+
+        restoran_with_status.append(
+            {
+                "restoran": restoran,
+                "status": status,
+                "jam_buka": jam_buka.strftime("%H:%M"),
+                "jam_tutup": jam_tutup.strftime("%H:%M"),
+            }
+        )
+
+    return render(request, "landing/index.html", {"restoran": restoran_with_status})
