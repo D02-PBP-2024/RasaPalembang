21fbc684a986dcf13c3a664e8262a6b061f32cb7
mdlnecg <mdlnecg@gmail.com>
2024-10-27 02:02:56 +0700

    feat: update design

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from forum.views import show_forum, show_forum_by_id, create_forum, delete_forum, delete_balasan, edit_balasan, edit_forum, balas_ajax, show_balasan
+from forum.views import show_forum, show_forum_by_id, create_forum, delete_forum, delete_balasan, edit_balasan, edit_forum, balas, show_balasan
 
 app_name = "forum"
 
@@ -11,6 +11,6 @@ urlpatterns = [
     path('<uuid:id_forum>/delete_forum', delete_forum, name='delete_forum'),
     path('<uuid:id_forum>/edit_balasan/<uuid:id_balasan>', edit_balasan, name='edit_balasan'),
     path('<uuid:id_forum>/edit_forum', edit_forum, name="edit_forum"),
-    path('<uuid:id_forum>/balas_ajax', balas_ajax, name='balas_ajax'),
+    path('<uuid:id_forum>/balas', balas, name='balas'),
     path('<uuid:id_forum>/show_balasan', show_balasan, name='show_balasan'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from forum.models import Forum, Balasan
@@ -34,7 +34,7 @@ def create_forum(request, id_restoran):
     form = ForumForm(request.POST or None)
 
     if request.user.peran != 'pengulas':
-        return redirect('forum:show_forum', id_restoran=restoran.id)
+        return HttpResponseNotFound()
 
     if form.is_valid() and request.method == "POST":
         topik = form.save(commit=False)
@@ -56,6 +56,9 @@ def edit_forum(request, id_restoran, id_forum):
     form = ForumForm(request.POST, instance=forum)
     restoran = Restoran.objects.get(pk=id_restoran)
 
+    if request.user.peran != 'pengulas' or request.user.id != forum.user.id:
+        return HttpResponseNotFound()
+
     if request.method == "POST":
         if form.is_valid():
             form.save()
@@ -70,6 +73,9 @@ def delete_forum(request, id_restoran, id_forum):
     forum = Forum.objects.get(pk=id_forum)
     restoran = Restoran.objects.get(pk=id_restoran)
 
+    if request.user.peran != 'pengulas' or request.user.id != forum.user.id:
+        return HttpResponseNotFound()
+
     forum.delete()
     return redirect('forum:show_forum', id_restoran=restoran.id)
 
@@ -81,8 +87,8 @@ def edit_balasan(request, id_restoran, id_forum, id_balasan):
     balasan = Balasan.objects.get(pk=id_balasan)
     form = BalasanForm(request.POST, instance=balasan)
 
-    if request.user.peran != 'pengulas':
-        return redirect('forum:show_forum_by_id', id_restoran=restoran.id, id_forum=forum.id)
+    if request.user.peran != 'pengulas' or request.user != balasan.user:
+        return HttpResponseNotFound()
     
     if request.method == "POST":
         if form.is_valid():
@@ -99,8 +105,8 @@ def delete_balasan(request, id_restoran, id_forum, id_balasan):
     restoran = Restoran.objects.get(pk=id_restoran)
     balasan = Balasan.objects.get(pk=id_balasan)
 
-    if request.user.peran != 'pengulas':
-        return redirect('forum:show_forum_by_id', id_restoran=restoran.id, id_forum=forum.id)
+    if request.user.peran != 'pengulas' or request.user != balasan.user:
+        return HttpResponseNotFound()
 
     balasan.delete()
     return redirect('forum:show_forum_by_id', id_restoran=restoran.id, id_forum=forum.id)
@@ -108,7 +114,7 @@ def delete_balasan(request, id_restoran, id_forum, id_balasan):
 
 @require_POST
 @login_required(login_url="login")
-def balas_ajax(request, id_restoran, id_forum):
+def balas(request, id_restoran, id_forum):
     pesan = request.POST.get("pesan")
     user = request.user
     forum = Forum.objects.get(pk=id_forum)
@@ -121,7 +127,7 @@ def balas_ajax(request, id_restoran, id_forum):
 
 def show_balasan(request, id_restoran, id_forum):
     try:
-        data = Balasan.objects.filter(forum=id_forum)
+        data = Balasan.objects.filter(forum=id_forum).order_by("tanggal_posting")
         balasan = []
         for item in data:
             balasan.append({
