ea81149fa83dbf43a67b009b95f4e3e7c9c1b844
AthallahD <athallah2209@gmail.com>
2024-12-02 16:30:08 +0700

    feat : add utils.py

---
+++ makanan/utils.py
@@ -0,0 +1,41 @@
+import re
+
+
+# Method untuk mengembalikan data makanan
+def makanan_data(makanan, message=None):
+    data = {
+        "pk": makanan.pk,
+        "fields": {
+            "nama": makanan.nama,
+            "harga": makanan.harga,
+            "deskripsi": makanan.deskripsi,
+            "gambar": makanan.gambar.url if makanan.gambar else "",
+            "kalori": makanan.kalori,
+            "restoran": makanan.restoran.pk,
+            "kategori": makanan.kategori.pk,
+        }
+    }
+
+    if message is not None:
+        data["message"] = message
+
+    return data
+
+def validasi_input(harga, kalori):
+    # Harga yang valid adalah 0 dan bilangan bulat positif
+    harga_pattern = r"^[0-9]\d*$"
+    # Kalori yang valid adalah bilangan bulat positif
+    kalori_pattern = r"^[0-9]\d*$"
+
+    # Memastikan input harga dan kalori valid
+    message = ""
+    if not re.match(harga_pattern, harga):
+        message += "harga"
+    
+    if not re.match(kalori_pattern, kalori):
+        if message != "":
+            message += " dan kalori"
+        else:
+            message += "kalori"
+    
+    return message
