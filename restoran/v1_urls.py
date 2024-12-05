9984d059137ac43df5253e9f1c52ff747eb91123
AthallahD <athallah2209@gmail.com>
2024-12-05 10:47:28 +0700

    feat : add module makanan urls in restoran

--- restoran/v1_urls.py
+++ restoran/v1_urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from forum.v1_views import forum_by_restoran
 from minuman.v1_views import minuman_by_restoran
+from makanan.v1_views import makanan_by_restoran
 
 app_name = "v1_restoran"
 
 urlpatterns = [
     path("<uuid:id_restoran>/minuman/", minuman_by_restoran, name="minuman_by_restoran"),
-    path("<uuid:id_restoran>/forum/", forum_by_restoran, name="forum_by_restoran")
+    path("<uuid:id_restoran>/forum/", forum_by_restoran, name="forum_by_restoran"),
+    path("<uuid:id_restoran>/makanan/", makanan_by_restoran, name="makanan_by_restoran"),
 ]
