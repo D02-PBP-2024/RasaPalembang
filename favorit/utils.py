9ee0aebe9df47e4e550acc03bea37954e6f6eb2f
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2024-12-09 15:39:53 +0700

    feat: everything about favorit?

--- favorit/utils.py
+++ favorit/utils.py
@@ -1,14 +1,14 @@
-# Method untuk mengembalikan data minuman
+# Method untuk mengembalikan data favorit
 def favorit_data(favorit, message=None):
     data = {
-            "pk": favorit.id,
-            "fields": {
-                "catatan": favorit.catatan,
-                "user": favorit.user.id,
-                "makanan": favorit.makanan.id if favorit.makanan else None,
-                "minuman": favorit.minuman.id if favorit.minuman else None,
-                "restoran": favorit.restoran.id if favorit.restoran else None,
-            }
+        "pk": favorit.id,
+        "fields": {
+            "catatan": favorit.catatan,
+            "user": favorit.user.id,
+            "makanan": favorit.makanan.id if favorit.makanan else None,
+            "minuman": favorit.minuman.id if favorit.minuman else None,
+            "restoran": favorit.restoran.id if favorit.restoran else None,
+        }
     }
 
     if message is not None:
--- favorit/v1_views.py
+++ favorit/v1_views.py
@@ -1,3 +1,4 @@
+import json
 from django.core.exceptions import ObjectDoesNotExist
 from favorit.forms import FavoritForm
 from restoran.models import Restoran
@@ -7,7 +8,6 @@ from minuman.models import Minuman
 from django.http import JsonResponse
 from favorit.utils import favorit_data
 from django.views.decorators.csrf import csrf_exempt
-import json
 
 
 @csrf_exempt
@@ -15,16 +15,15 @@ def favorit(request):
     """
     API endpoint to show the list of favorites for the logged-in user.
     """
-    
-    # Memastikan user terautentikasi
-    if not request.user.is_authenticated:
-        return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
-        
-    # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
-    if request.user.peran not in ["pengulas", "pemilik_restoran"]:
-        return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
-        
     if request.method == "GET":
+        # Memastikan user terautentikasi
+        if not request.user.is_authenticated:
+            return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
+
+        # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
+        if request.user.peran not in ["pengulas", "pemilik_restoran"]:
+            return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
+
         # Mengambil seluruh objek favorit
         favorit = Favorit.objects.filter(user=request.user)
 
@@ -35,64 +34,94 @@ def favorit(request):
         return JsonResponse(data, safe=False, status=200)
     else:
         return JsonResponse({"message": "Method tidak diizinkan."}, status=405)
-        
+
+
 @csrf_exempt
 def favorit_by_id(request, id_favorit):
     """
     API endpoint to handle GET, PUT, and DELETE requests for a favorite item by ID.
     """
-    if not request.user.is_authenticated:
-        return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
-    
-    # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
-    if request.user.peran not in ["pengulas", "pemilik_restoran"]:
-        return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
-
-    try:
-        favorit = Favorit.objects.get(id=id_favorit, user=request.user)
-    except ObjectDoesNotExist:
-        return JsonResponse({"error": "Favorit tidak ditemukan."}, status=404)
-
     if request.method == "GET":
-        # Mengambil seluruh objek favorit
-        favorit = Favorit.objects.all()
+        # Memastikan user terautentikasi
+        if not request.user.is_authenticated:
+            return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
+
+            # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
+        if request.user.peran not in ["pengulas", "pemilik_restoran"]:
+            return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
+
+        try:
+            favorit = Favorit.objects.get(id=id_favorit, user=request.user)
+        except ObjectDoesNotExist:
+            return JsonResponse({"message": "Favorit tidak ditemukan."}, status=404)
+
         # Mengembalikan data seluruh favorit
-        data = []
-        for m in favorit:
-            data.append(favorit_data(m))
-        return JsonResponse(data, safe=False, status=200)
+        data = favorit_data(favorit)
+        return JsonResponse(data, status=200)
 
     elif request.method == "PUT":
+        # Memastikan user terautentikasi
+        if not request.user.is_authenticated:
+            return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
+
+        # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
+        if request.user.peran not in ["pengulas", "pemilik_restoran"]:
+            return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
+
         try:
             data = json.loads(request.body)
         except json.JSONDecodeError:
-            return JsonResponse({"error": "Input tidak valid."}, status=400)
+            return JsonResponse({"message": "Input tidak valid."}, status=400)
+
+        try:
+            favorit = Favorit.objects.get(id=id_favorit, user=request.user)
+        except ObjectDoesNotExist:
+            return JsonResponse({"message": "Favorit tidak ditemukan."}, status=404)
 
         form = FavoritForm(data, instance=favorit)
         if form.is_valid():
             form.save()
-            return JsonResponse({"status": "success"}, status=200)
-        return JsonResponse({"error": form.errors}, status=400)
+
+            data = favorit_data(favorit)
+            return JsonResponse(data, status=200)
+        return JsonResponse({"message": form.errors}, status=400)
 
     elif request.method == "DELETE":
-        favorit.delete()
-        return JsonResponse({"status": "success"}, status=200)
+        # Memastikan user terautentikasi
+        if not request.user.is_authenticated:
+            return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
 
-    return JsonResponse({"error": "Favorit tidak ditemukan."}, status=404)
+            # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
+        if request.user.peran not in ["pengulas", "pemilik_restoran"]:
+            return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
+
+        try:
+            favorit = Favorit.objects.get(id=id_favorit, user=request.user)
+        except ObjectDoesNotExist:
+            return JsonResponse({"message": "Favorit tidak ditemukan."}, status=404)
+
+        data = favorit_data(favorit)
+
+        favorit.delete()
+        return JsonResponse(data, status=200)
+
+    return JsonResponse({"message": "Method tidak diizinkan."}, status=405)
+
 
 @csrf_exempt
 def favorit_by_makanan(request, id_makanan):
     """
     API endpoint to add a makanan to the user's favorites.
     """
-    if not request.user.is_authenticated:
-        return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
-    
-    # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
-    if request.user.peran not in ["pengulas", "pemilik_restoran"]:
-        return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
-
     if request.method == "POST":
+        # Memastikan user terautentikasi
+        if not request.user.is_authenticated:
+            return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
+
+        # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
+        if request.user.peran not in ["pengulas", "pemilik_restoran"]:
+            return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
+
         try:
             # Mengambil objek `Makanan` menggunakan id_makanan
             item = Makanan.objects.get(pk=id_makanan)
@@ -102,23 +131,30 @@ def favorit_by_makanan(request, id_makanan):
 
         # Membuat atau mendapatkan instance favorit
         favorit, created = Favorit.objects.get_or_create(user=request.user, makanan=item)
-        return JsonResponse({"status": "success", "created": created}, status=200)
-    
-    return JsonResponse({"message": "Method tidak diizinkan."}, status=405)   
+
+        if not created:
+            return JsonResponse({"message": "Makanan sudah ditambahkan."}, status=400)
+
+        data = favorit_data(favorit)
+        return JsonResponse(data, status=201)
+
+    return JsonResponse({"message": "Method tidak diizinkan."}, status=405)
+
 
 @csrf_exempt
 def favorit_by_minuman(request, id_minuman):
     """
     API endpoint to add a minuman to the user's favorites.
     """
-    if not request.user.is_authenticated:
-        return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
-    
-    # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
-    if request.user.peran not in ["pengulas", "pemilik_restoran"]:
-        return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
-
     if request.method == "POST":
+        # Memastikan user terautentikasi
+        if not request.user.is_authenticated:
+            return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
+
+            # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
+        if request.user.peran not in ["pengulas", "pemilik_restoran"]:
+            return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
+
         try:
             # Coba mendapatkan objek Minuman
             item = Minuman.objects.get(pk=id_minuman)
@@ -128,31 +164,43 @@ def favorit_by_minuman(request, id_minuman):
 
         # Tambahkan minuman ke favorit pengguna
         favorit, created = Favorit.objects.get_or_create(user=request.user, minuman=item)
-        return JsonResponse({"status": "success", "created": created}, status=200)
-    
-    return JsonResponse({"error": "Method tidak diizinkan."}, status=405)    
+
+        if not created:
+            return JsonResponse({"message": "Minuman sudah ditambahkan."}, status=400)
+
+        data = favorit_data(favorit)
+        return JsonResponse(data, status=201)
+
+    return JsonResponse({"message": "Method tidak diizinkan."}, status=405)
+
 
 @csrf_exempt
 def favorit_by_restoran(request, id_restoran):
     """
     API endpoint to add a restoran to the user's favorites.
     """
-    if not request.user.is_authenticated:
-        return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
-    
-    # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
-    if request.user.peran not in ["pengulas", "pemilik_restoran"]:
-        return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
-
     if request.method == "POST":
+        # Memastikan user terautentikasi
+        if not request.user.is_authenticated:
+            return JsonResponse({"message": "User tidak terautentikasi."}, status=401)
+
+        # Memastikan peran user adalah `pengulas` atau `pemilik_restoran`
+        if request.user.peran not in ["pengulas", "pemilik_restoran"]:
+            return JsonResponse({"message": "Tindakan tidak diizinkan."}, status=403)
+
         try:
             # Mencoba untuk mendapatkan objek Restoran berdasarkan ID
             item = Restoran.objects.get(pk=id_restoran)
         except ObjectDoesNotExist:
-            return JsonResponse({"error": "Restoran tidak ditemukan."}, status=404)
+            return JsonResponse({"message": "Restoran tidak ditemukan."}, status=404)
 
         # Membuat atau mendapatkan objek Favorit
         favorit, created = Favorit.objects.get_or_create(user=request.user, restoran=item)
-        return JsonResponse({"status": "success", "created": created}, status=200)
 
-    return JsonResponse({"error": "Method tidak diizinkan."}, status=405)
+        if not created:
+            return JsonResponse({"message": "Restoran sudah ditambahkan."}, status=400)
+
+        data = favorit_data(favorit)
+        return JsonResponse(data, status=201)
+
+    return JsonResponse({"message": "Method tidak diizinkan."}, status=405)
