b484ee2ea1f42f37b32b94d161c706374f7eee5b
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2024-12-19 01:18:25 +0700

    feat: oke user pake fields juga

--- authentication/utils.py
+++ authentication/utils.py
@@ -2,13 +2,15 @@
 def user_data(user, message=None):
     data = {
         "pk": user.pk,
-        "username": user.username,
-        "nama": user.nama,
-        "deskripsi": user.deskripsi,
-        "peran": user.peran,
-        "foto": user.foto.url if user.foto else "",
-        "poin": user.poin,
-        "date_joined": user.date_joined,
+        "fields": {
+            "username": user.username,
+            "nama": user.nama,
+            "deskripsi": user.deskripsi,
+            "peran": user.peran,
+            "foto": user.foto.url if user.foto else "",
+            "poin": user.poin,
+            "date_joined": user.date_joined,
+        }
     }
     if message is not None:
         data["message"] = message
